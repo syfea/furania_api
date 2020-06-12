08321c351680c2295f413f1b67174ccf6f78eb82
Sylvain Féasson <sfeasson@intercountry.com>
2020-06-12 13:57:40 +0200

    feat (User) : add createAt attribute

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,6 +76,11 @@ class User implements UserInterface
      */
     private $albums;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
     public function __construct()
     {
         $this->photos = new ArrayCollection();
@@ -245,6 +250,18 @@ class User implements UserInterface
                 $album->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
