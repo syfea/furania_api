acba8a01c6f808eca73fb1b0b2cbdd8d75cd7ca5
Sylvain Féasson <sfeasson@intercountry.com>
2020-06-12 14:02:51 +0200

    add current user in doctrine requests

---
+++ src/Doctrine/CurrentUserExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Doctrine;
+
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Album;
+use App\Entity\Photo;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private $security;
+    private $auth;
+
+    public function __construct(Security $security, AuthorizationCheckerInterface $checker)
+    {
+        $this->security = $security;
+        $this->auth = $checker;
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
+        $user = $this->security->getUser();
+        if (($resourceClass === Photo::class || $resourceClass === Album::class) && !$this->auth->isGranted('ROLE_ADMIN')) {
+            $rootAlias = $queryBuilder->getRootAliases()[0];
+            $queryBuilder->andWhere("$rootAlias.user = :user");
+            $queryBuilder->setParameter("user", $user);
+        }
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+}
